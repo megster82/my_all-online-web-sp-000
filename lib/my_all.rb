4240f03611d2ba33fe18112fea8d79dc41062ac3
meaghan jennings <meaghanljennings@gmail.com>
2021-03-02 16:53:48 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,6 +5,12 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
 
 #   block_return_values = []
